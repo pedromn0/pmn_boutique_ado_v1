2e8f59c17e8152831a3de1aaf302d7629fa95ae5
pedromn0 <muzzioneri@gmail.com>
2022-02-22 15:37:38 +0000

    Add adjust bag and remove from bag views, and calc subtotal template filter

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -35,3 +35,53 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """ Adjust the quantity of the specified product to the specified amount """
+
+    quantity = int(request.POST.get('quantity'))
+    seize = None
+    if 'product_size' in request.POST:
+        seize = request.POST['product_size']
+    bag = request.session.get('bag', {})
+
+    if seize:
+        if quantity > 0:
+            bag[item_id]['items_by_seize'][seize] = quantity
+        else:
+            del bag[item_id]['items_by_seize'][seize]
+            if not bag[item_id]['items_by_seize']:
+                bag.pop(item_id)
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        size = None
+        if 'product_size' in request.POST:
+            seize = request.POST['product_size']
+        bag = request.session.get('bag', {})
+
+        if size:
+            del bag[item_id]['items_by_seize'][seize]
+            if not bag[item_id]['items_by_seize']:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
+
